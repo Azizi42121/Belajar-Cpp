ad0dc81f891fb3f8139ea7a00616b532bc3c8696
Rojul Jundan Azizi <Rojulazizi@gmail.com>
2026-08-11 09:23:35 +0700

    deklarasi variabel

---
+++ variabel/variable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    // deklarasi variable
+    // variabel harus didefinisikan dulu tipe datanya apa
+    int a = 1; // jelaskan dahulu tipe datanya "int" baru kemudian diberi nama variabelnya "a" lalu di assign
+    int b; // mendeklarasikan variabel juga tidak perlu harus diassignmen terlebih dahulu, bisa dilakukan dengan deklarasikan dulu saja variabelnya
+
+    std::cout << a << std::endl;
+    return 0;
+}
