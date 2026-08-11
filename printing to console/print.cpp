68a24676fcc3b690ee52068bc3a597577e38cd54
Rojul Jundan Azizi <Rojulazizi@gmail.com>
2026-08-11 09:22:26 +0700

    display ke console

---
+++ printing to console/print.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Printing to Console" << std::endl;
+    /*
+        1. std -> itu standard library, di import menggunakan #include <iostream>
+        2. :: -> itu artinya mengambil sebuah kode dari library
+        3. cout -> artinya console out, digunakan untuk menampilkan ke display komputer
+        4. endl -> deklarasi bahwa dia adalah akhir dari baris kode tersebut, cout selanjutnya akan ditampilkan di line baru
+        5. ; -> penutup dari kode, menyatakan bahwa kode ini berakhir
+    */
+
+    std::cout << "Halo sodara-sodara sekalian\n";
+    std::cout << "Selamat pagi semua saudaraku ";
+    std::cout << "Salam sejahtera untuk kita semua" << std::endl;
+    std::cout << "Semoga kita selalu diberkati";
+    return 0; // artinya bahwa program berjalan dengan baik
+}
