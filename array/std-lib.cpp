7f5ea12cfc52a9b12fd3d7997503dcebb1d236c0
Rojul Jundan Azizi <Rojulazizi@gmail.com>
2026-08-19 08:48:43 +0700

    standard library untuk array di cpp

---
+++ array/std-lib.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <array> // standard library untuk array
+using namespace std;
+
+int main() {
+    // membuat array dengan menggunakan standard library
+    // array < int, jumlah array> nama array
+
+    array < int, 5 > nilai;
+
+    for (int i = 0; i <= 4; i++) {
+        nilai[i] = i;
+        cout << "nilai [" << i << "] = " << nilai[i];
+        cout << " address : " << &nilai[i] << endl;
+    }
+
+    cout << endl;
+    // ukuran array
+    cout << "ukuran : " << nilai.size() << endl;
+    // address awal dari array
+    cout << "address awal : " << nilai.begin() << endl;
+    // address akhir dari array
+    cout << "address akhir : " << nilai.end() << endl; // ini bukan address dari nilai terakhir, tapi batas akhir dari arraynya
+    // nilai dengan index
+    cout << "nilai ke 2 : " << nilai.at(2) << endl;
+
+    return 0;
+}
