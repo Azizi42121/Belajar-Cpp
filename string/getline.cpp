053d70ed77981457924d0a4e67a81d503c2ff8e5
Rojul Jundan Azizi <Rojulazizi@gmail.com>
2026-08-24 09:24:24 +0700

    getline in string

---
+++ string/getline.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string kalimat_input;
+
+    // getline(cin, variabel)
+    cout << "Masukkan kalimat: ";
+    getline(cin,kalimat_input);
+
+    cout << "input anda: ";
+
+    // jumlah kata dari input
+    int posisi = 0;
+    int jumlah = 0;
+
+    while(true) {
+        posisi = kalimat_input.find(" ", posisi + 1);
+        jumlah++;
+        if(posisi < 0) {
+            break;
+        }
+    }
+
+    cout << "jumlah kata : " << jumlah << "\n";
+
+    return 0;
+}
